239ba806dc04d300ea090225409a956cf1f0bdc0
JJ Garlitos <jj.garlitos@outlook.com>
2019-11-23 14:52:21 -0700

    Populated Locations through migration

---
+++ BatmansInventory.API/Migrations/20191123213641_PopulateLocationsTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatmansInventory.API.Migrations
+{
+    public partial class PopulateLocationsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Locations (City, State) VALUES ('Boise', 'Idaho')");
+            migrationBuilder.Sql("INSERT INTO Locations (City, State) VALUES ('Lansing', 'Michigan')");
+            migrationBuilder.Sql("INSERT INTO Locations (City, State) VALUES ('Chicago', 'Illinois')");
+            migrationBuilder.Sql("INSERT INTO Locations (City, State) VALUES ('Bonita Springs', 'Florida')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

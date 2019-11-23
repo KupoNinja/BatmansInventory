94e7ac454d37fbf36d42fbedd6e15a24903232a0
JJ Garlitos <jj.garlitos@outlook.com>
2019-11-23 16:52:21 -0700

    Populated Items through migration

---
+++ BatmansInventory.API/Migrations/20191123233816_PopulateItemsTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BatmansInventory.API.Migrations
+{
+    public partial class PopulateItemsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Items (PartName, PartNumber, OrderLeadTime, QuantityOnHand, SafetyStock, Created, CreatedBy) " +
+                                            "VALUES ('Batarang', 'BTG-001', 3, 10, 4, '11-23-2019', 'Lucius Fox')");
+            migrationBuilder.Sql("INSERT INTO Items (PartName, PartNumber, OrderLeadTime, QuantityOnHand, SafetyStock, Created, CreatedBy) " +
+                                            "VALUES ('Bat-Bolas', 'BBO-102', 5, 7, 2, '11-23-2019', 'Lucius Fox')");
+            migrationBuilder.Sql("INSERT INTO Items (PartName, PartNumber, OrderLeadTime, QuantityOnHand, SafetyStock, Created, CreatedBy) " +
+                                            "VALUES ('Flash-Bang Grenades', 'FBG-081', 6, 12, 5, '11-22-2019', 'Alfred Pennyworth')");
+            migrationBuilder.Sql("INSERT INTO Items (PartName, PartNumber, OrderLeadTime, QuantityOnHand, SafetyStock, Created, CreatedBy) " +
+                                            "VALUES ('Grapple Gun', 'GGU-307', 10, 1, 2, '11-22-2019', 'Alfred Pennyworth')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

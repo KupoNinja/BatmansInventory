a4b40f2f0fd8bb0347e8dff8f92f338a4bd7a3e3
JJ Garlitos <jj.garlitos@outlook.com>
2019-11-24 23:55:39 -0700

    Attempted to create first test w/ Moq.

--- BatmansInventory.Tests/ItemsShould.cs
+++ BatmansInventory.Tests/ItemsShould.cs
@@ -1,4 +1,8 @@
+using BatmansInventory.API.Interfaces;
+using BatmansInventory.API.Models;
 using BatmansInventory.API.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using Xunit;
 
@@ -7,9 +11,42 @@ namespace BatmansInventory.Tests
     public class ItemsShould
     {
         [Fact]
+        public void CreateNewItem()
+        {
+            //UseInMemoryDatabase not an extension method?
+            //var options = new DbContextOptionsBuilder<DataContext>().Use
+            //    .UseInMemoryDatabase(databaseName: "BatmansInventoryDatabase")
+            //    .O
+
+            //Arrange
+            var mockSet = new Mock<DbSet<Item>>();
+            var mockContext = new Mock<DataContext>();
+            mockContext.Setup(c => c.Items).Returns(mockSet.Object);
+            Item item = new Item()
+            {
+                ItemId = 5,
+                PartName = "Bat Test",
+                PartNumber = "BTE-747",
+                OrderLeadTime = 6,
+                QuantityOnHand = 4,
+                SafetyStock = 5,
+                Created = DateTime.Now,
+                CreatedBy = "Tester",
+            };
+
+            //Act - Create Item
+            var repo = new ItemsService(mockContext.Object);
+            repo.CreateItem(item);
+
+            //Assert
+            mockSet.Verify(m => m.Add(It.IsAny<Item>()), Times.Once);
+            mockContext.Verify(m => m.SaveChanges(), Times.Once);
+        }
+
+        [Fact]
         public void RetrieveItemsUnderSafetyStock()
         {
-            
+            var mockItemsService = new Mock<IItemsService>();
         }
     }
 }
--- BatmansInventory.Tests/PhysicalItemsShould.cs
+++ BatmansInventory.Tests/PhysicalItemsShould.cs
@@ -8,22 +8,14 @@ namespace BatmansInventory.Tests
 {
     public class PhysicalItemsShould
     {
-        private readonly DataContext _db;
-
         [Fact]
         public void ImplementIPhysicalItem()
         {
-            PhysicalItem sut = new PhysicalItem();
-
-            Assert.IsAssignableFrom<IPhysicalItem>(sut);
         }
 
         [Fact]
         public void GetTotalValue()
         {
-            PhysicalItemsService sut = new PhysicalItemsService(_db);
-
-            Assert.NotEqual(0, sut.GetTotalValueByItem(3));
         }
     }
 }

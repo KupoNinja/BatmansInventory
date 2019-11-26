cd45f32bc3022720a2734558bc23409494ad6e8b
JJ Garlitos <jj.garlitos@outlook.com>
2019-11-25 23:57:48 -0700

    Broken Test. Can now "mock DbContext tho.

--- BatmansInventory.Tests/ItemsShould.cs
+++ BatmansInventory.Tests/ItemsShould.cs
@@ -5,49 +5,61 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using Moq.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BatmansInventory.Tests
 {
     public class ItemsShould
     {
-        [Fact]
-        public void CreateNewItem()
-        {
-            //UseInMemoryDatabase not an extension method?
-            //var options = new DbContextOptionsBuilder<DataContext>().Use
-            //    .UseInMemoryDatabase(databaseName: "BatmansInventoryDatabase")
-            //    .O
+        //private DbContextOptionsBuilder<DataContext> _options;
 
-            //Arrange
-            var mockSet = new Mock<DbSet<Item>>();
-            var mockContext = new Mock<DataContext>();
-            mockContext.Setup(c => c.Items).Returns(mockSet.Object);
-            Item item = new Item()
-            {
-                ItemId = 5,
-                PartName = "Bat Test",
-                PartNumber = "BTE-747",
-                OrderLeadTime = 6,
-                QuantityOnHand = 4,
-                SafetyStock = 5,
-                Created = DateTime.Now,
-                CreatedBy = "Tester",
-            };
+        //public ItemsShould()
+        //{
+        //    var options = new DbContextOptionsBuilder<DataContext>()
+        //    .UseInMemoryDatabase(databaseName: "BatmansInventoryDatabase").Options;
+        //}
 
-            //Act - Create Item
-            var repo = new ItemsService(mockContext.Object);
-            repo.CreateItem(item);
+        //[Fact]
+        //public void CreateNewItem()
+        //{
+        //    Item item = new Item()
+        //    {
+        //        ItemId = 5,
+        //        PartName = "Bat Test",
+        //        PartNumber = "BTE-747",
+        //        OrderLeadTime = 6,
+        //        QuantityOnHand = 4,
+        //        SafetyStock = 5,
+        //        Created = DateTime.Now,
+        //        CreatedBy = "Tester",
+        //    };
 
-            //Assert
-            mockSet.Verify(m => m.Add(It.IsAny<Item>()), Times.Once);
-            mockContext.Verify(m => m.SaveChanges(), Times.Once);
-        }
+        //    //Act - Create Item
+        //    var repo = new ItemsService(mockContext.Object);
+        //    repo.CreateItem(item);
+
+        //    //Assert
+        //    mockSet.Verify(m => m.Add(It.IsAny<Item>()), Times.Once);
+        //    mockContext.Verify(m => m.SaveChanges(), Times.Once);
+        //}
 
         [Fact]
         public void RetrieveItemsUnderSafetyStock()
         {
-            var mockItemsService = new Mock<IItemsService>();
+            //Arrange
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "BatmansInventoryDatabase").Options;
+            var mock = new Mock<Item>();
+            Item item = mock.Object;
+
+            //Act
+            var context = new DataContext(options);
+            var repo = new ItemsService(context);
+
+            //Assert
+            Assert.IsType(repo.GetAllUnderSafetyStock(),
+                typeof(List<item>));
         }
     }
 }

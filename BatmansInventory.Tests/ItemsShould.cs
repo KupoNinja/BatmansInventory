c69c832c4bb9ee2cbbec547bf57c9b0bf6d91d3f
JJ Garlitos <jj.garlitos@outlook.com>
2019-11-27 22:41:30 -0700

    First successful test!

--- BatmansInventory.Tests/ItemsShould.cs
+++ BatmansInventory.Tests/ItemsShould.cs
@@ -44,21 +44,59 @@ namespace BatmansInventory.Tests
         //}
 
         [Fact]
-        public void RetrieveItemsUnderSafetyStock()
+        public void RetrieveListOfItemsUnderSafetyStock()
         {
             //Arrange
             var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: "BatmansInventoryDatabase").Options;
-            var mock = new Mock<Item>();
-            Item item = mock.Object;
+                .UseInMemoryDatabase("BatmansInventoryDatabase").Options;
+            using var context = new DataContext(options);
+            Item fakeItemUnderSafetyStock1 = new Item()
+            {
+                PartName = "BataTest",
+                PartNumber = "BTE-321",
+                OrderLeadTime = 6,
+                QuantityOnHand = 5,
+                SafetyStock = 10,
+                Created = DateTime.Now,
+                CreatedBy = "Alfred"
+            };
+            Item fakeItemUnderSafetyStock2 = new Item()
+            {
+                PartName = "BataTest1",
+                PartNumber = "BTE-123",
+                OrderLeadTime = 4,
+                QuantityOnHand = 2,
+                SafetyStock = 5,
+                Created = DateTime.Now,
+                CreatedBy = "Alfred"
+            };
+            Item fakeItemNotUnderSafetyStock = new Item()
+            {
+                PartName = "Safe",
+                PartNumber = "SAF-321",
+                OrderLeadTime = 6,
+                QuantityOnHand = 12,
+                SafetyStock = 7,
+                Created = DateTime.Now,
+                CreatedBy = "Alfred"
+            };
+            context.Add(fakeItemUnderSafetyStock1);
+            context.Add(fakeItemUnderSafetyStock2);
+            context.Add(fakeItemNotUnderSafetyStock);
+            context.SaveChanges();
+
+            ItemsService repo = new ItemsService(context);
 
             //Act
-            var context = new DataContext(options);
-            var repo = new ItemsService(context);
+            var fakeListItemsUnderSafetyStock = repo.GetAllUnderSafetyStock();
 
             //Assert
-            Assert.IsType(repo.GetAllUnderSafetyStock(),
-                typeof(List<Item>));
+            //Multiple asserts Ok?
+            Assert.Contains(fakeItemUnderSafetyStock1, fakeListItemsUnderSafetyStock);
+            Assert.Contains(fakeItemUnderSafetyStock2, fakeListItemsUnderSafetyStock);
+            Assert.DoesNotContain(fakeItemNotUnderSafetyStock, fakeListItemsUnderSafetyStock);
         }
+
+        
     }
 }
